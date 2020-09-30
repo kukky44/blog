5c60f8db8a9a30c797c793509aee037b0d68eb00
kukky44 <yusuke122644@gmail.com>
2020-09-30 18:56:28 +0900

    TODOの更新

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import SEO from "../components/SEO";
 import PostList from "../components/PostList";
 
 // TODO: 本文の余白の見直し
-// TODO: 検索機能
 // TODO: プロフィールのSNS icon
 // TODO: footer の作成
 // TODO: ロゴ作成
+// TODO: 本文の画像埋め込み実装
 
 const IndexPage = ({ data }) => {
   const posts = data.allMdx.edges;
